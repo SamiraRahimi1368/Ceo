f850e1ebda846c06d3065d85017f91626f5e9121
Samira <samira.rahimi1998@gmail.com>
2023-05-20 18:15:14 -0400

    p

--- app/faq/page.js
+++ app/faq/page.js
@@ -1,21 +1,54 @@
-const questions = [
-  {
-    id: 1,
-    question: "why",
-    answer: "because",
-  },
-  {
-    id: 2,
-    question: "how",
-    answer: "like that",
-  },
-];
+"use client";
+import { useState, useEffect } from "react";
+
 const Faq = () => {
+  const [questions, setQuestions] = useState([
+    {
+      id: 1,
+      question: "why",
+      answer: "because",
+    },
+    {
+      id: 2,
+      question: "how",
+      answer: "like that",
+    },
+  ]);
+  const openItem = (item) => {
+    setQuestions((previousQuestions) => {
+      for (var index in previousQuestions) {
+        const question = previousQuestions[index];
+        if (questions.id === item.id) {
+          question.open = true;
+        } else {
+          question.open = false;
+        }
+      }
+      return previousQuestions;
+    });
+  };
+
+  const addMore = () => {
+    setQuestions((previousQuestions) => {
+      return [
+        {
+          id: 3,
+          question: " where",
+          answer: "here",
+        },
+        ...previousQuestions,
+      ];
+    });
+  };
   return (
     <div>
       {questions.map((item) => (
-        <div>{item.question}</div>
+        <div onClick={() => openItem(item)}>
+          {item.question}
+          {item.open && <div>{item.answer}</div>}
+        </div>
       ))}
+      <button onClick={addMore}> Add More </button>
     </div>
   );
 };
