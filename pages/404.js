0dd1b3a53650d241666d7254ec419f40fbc766d5
Samira <samira.rahimi1998@gmail.com>
2023-06-17 13:31:09 -0400

    update

---
+++ pages/404.js
@@ -0,0 +1,11 @@
+const NotFound = () => {
+    return (
+      <>
+        <h2>Not Found</h2>
+        <p>Could not find the requested resource</p>
+      </>
+    );
+  };
+  
+  export default NotFound;
+  
